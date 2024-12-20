1605e4ec753bbc9f7567eca604bed07e55ec1633
investiatech <investia.tech@gmail.com>
2024-12-20 17:46:33 +0100

    correct highlight section

--- app/components/highlights.tsx
+++ app/components/highlights.tsx
@@ -1,9 +1,21 @@
+import { MessageSquareLockIcon } from 'lucide-react';
+import { MessageCircleHeart } from 'lucide-react';
+import { MessageSquareCode } from 'lucide-react';
+import { MessageCircleMore } from 'lucide-react';
+import { MessageSquareDashedIcon } from 'lucide-react';
+import { MessageSquareDotIcon } from 'lucide-react';
+
 export default function Highlights() {
     return (
         <section className="bg-white">
             <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
                 <div className="space-y-8 md:grid md:grid-cols-2 text-center lg:grid-cols-3 md:gap-y-16 md:gap-x-12 md:space-y-0">
-                    <div>
+                    <div className='text-center md:items-start md:text-center lg:block mb-8'>
+                        <div className='md:flex-shrink-0 flex justify-center'>
+                            <div className='h-16 w-16 flex items-center justify-center mb-6 rounded-full border bg-white text-blue-500'>
+                                <MessageSquareCode />
+                            </div>
+                        </div>
                         <h3 className="mb-2 text-xl font-bold dark:text-white">Strategiczne Decyzje</h3>
                         <p className="text-gray-500 dark:text-gray-400">Podejmuj kluczowe decyzje polityczne, budując swoją kampanię wyborczą i zdobywając poparcie wyborców.</p>
                         {/* <a href="#" className="inline-flex items-center mt-2 font-thin text-sm text-blue-500 hover:text-blue-800">
@@ -11,7 +23,13 @@ export default function Highlights() {
                             <svg className="ml-1 w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
                         </a> */}
                     </div>
-                    <div>
+                    <div className='text-center md:items-start md:text-center lg:block mb-8'>
+
+                        <div className='md:flex-shrink-0 flex justify-center'>
+                            <div className='h-16 w-16 flex items-center justify-center mb-6 rounded-full border bg-white text-blue-500'>
+                                <MessageCircleHeart />
+                            </div>
+                        </div>
                         <h3 className="mb-2 text-xl font-bold dark:text-white">Rywalizacja z Przeciwnikami</h3>
                         <p className="text-gray-500 dark:text-gray-400">Zmagaj się z rywalami, którzy nie odpuszczą. Gra oferuje różnorodne postacie polityczne, z którymi musisz konkurować o poparcie wyborców. Każdy przeciwnik ma swoją strategię.</p>
                         {/* <a href="#" className="inline-flex items-center mt-2 font-thin text-sm text-blue-500 hover:text-blue-800">
@@ -19,7 +37,13 @@ export default function Highlights() {
                             <svg className="ml-1 w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
                         </a> */}
                     </div>
-                    <div>
+                    <div className='text-center md:items-start md:text-center lg:block mb-8'>
+
+                        <div className='md:flex-shrink-0 flex justify-center'>
+                            <div className='h-16 w-16 flex items-center justify-center mb-6 rounded-full border bg-white text-blue-500'>
+                                <MessageCircleMore />
+                            </div>
+                        </div>
                         <h3 className="mb-2 text-xl font-bold dark:text-white">Dynamiczna Gra</h3>
                         <p className="text-gray-500 dark:text-gray-400">Zachowanie wyborców, nastroje społeczne i media zmieniają się w zależności od Twoich działań.</p>
                         {/* <a href="#" className="inline-flex items-center mt-2 font-thin text-sm text-blue-500 hover:text-blue-800">
@@ -27,7 +51,13 @@ export default function Highlights() {
                             <svg className="ml-1 w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
                         </a> */}
                     </div>
-                    <div>
+                    <div className='text-center md:items-start md:text-center lg:block mb-8'>
+
+                        <div className='md:flex-shrink-0 flex justify-center'>
+                            <div className='h-16 w-16 flex items-center justify-center mb-6 rounded-full border bg-white text-blue-500'>
+                                <MessageSquareDashedIcon />
+                            </div>
+                        </div>
                         <h3 className="mb-2 text-xl font-bold dark:text-white">Pełna Personalizacja Twojego Kandydata i Zespołu</h3>
                         <p className="text-gray-500 dark:text-gray-400">Twórz swojego kandydata od podstaw! Dostosuj wygląd, osobowość, poglądy polityczne i sposób komunikacji. Oprócz tego, zarządzaj zespołem doradców i ekspertów, którzy pomogą Ci w zdobywaniu głosów i budowaniu zwycięskiej strategii.</p>
                         {/* <a href="#" className="inline-flex items-center mt-2 font-thin text-sm text-blue-500 hover:text-blue-800">
@@ -35,7 +65,13 @@ export default function Highlights() {
                             <svg className="ml-1 w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
                         </a> */}
                     </div>
-                    <div>
+                    <div className='text-center md:items-start md:text-center lg:block mb-8'>
+
+                        <div className='md:flex-shrink-0 flex justify-center'>
+                            <div className='h-16 w-16 flex items-center justify-center mb-6 rounded-full border bg-white text-blue-500'>
+                                <MessageSquareDotIcon />
+                            </div>
+                        </div>
                         <h3 className="mb-2 text-xl font-bold dark:text-white">Realistyczna Symulacja Wyborów</h3>
                         <p className="text-gray-500 dark:text-gray-400">Przeżyj ekscytującą symulację wyborów prezydenckich, podejmując decyzje, które mają wpływ na losy kraju.</p>
                         {/* <a href="#" className="inline-flex items-center mt-2 font-thin text-sm text-blue-500 hover:text-blue-800">
@@ -43,7 +79,13 @@ export default function Highlights() {
                             <svg className="ml-1 w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
                         </a> */}
                     </div>
-                    <div>
+                    <div className='text-center md:items-start md:text-center lg:block mb-8'>
+
+                        <div className='md:flex-shrink-0 flex justify-center'>
+                            <div className='h-16 w-16 flex items-center justify-center mb-6 rounded-full border bg-white text-blue-500'>
+                                <MessageSquareLockIcon />
+                            </div>
+                        </div>
                         <h3 className="mb-2 text-xl font-bold dark:text-white">Dostępna na iOS i Androida</h3>
                         <p className="text-gray-500 dark:text-gray-400">Ciesz się grą wszędzie dzięki pełnej kompatybilności z urządzeniami iOS i Android – weź udział w wyborach w dowolnym miejscu!</p>
                         {/* <a href="#" className="inline-flex items-center mt-2 font-thin text-sm text-blue-500 hover:text-blue-800">
